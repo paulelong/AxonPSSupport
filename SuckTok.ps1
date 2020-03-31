c2ffc6066755b2177d529a45ac0181774ab5182b
Paul E. Long <paulelong@outlook.com>
2020-03-31 14:24:20 -0400

    Saved info in a dictionary with the device ID as key

--- SuckTok.ps1
+++ SuckTok.ps1
@@ -2,14 +2,23 @@ $Serial = read-host
 read-host
 $Firmware = read-host
 read-host
-$ootpw = read-host
+$rootpw = read-host
 read-host
 $SSHpw = read-host
 read-host
 $Expires = read-host
 
-$global:ser = $Serial.SubString($Serial.LastIndexOf(": ") + 2)
-$global:fw = $Firmware.SubString($Firmware.LastIndexOf(": ") + 2)
-$global:supw = $ootpw.SubString($ootpw.LastIndexOf(": ") + 2)
-$global:sshpw = $SSHpw.SubString($SSHpw.LastIndexOf(": ") + 2)
-$global:ex = $Expires.SubString($Expires.LastIndexOf(": ") + 2)
+$DevID = $Serial.SubString($Serial.LastIndexOf(": ") + 2)
+
+if($global:firmware.Count -le 0)
+{
+     $global:firmware = @{} 
+     $global:supw = @{}
+     $global:sshpw = @{}
+     $global:ex = @{}    
+}
+
+$global:firmware[$DevID] = $Firmware.SubString($Firmware.LastIndexOf(": ") + 2)
+$global:supw[$DevID] = $rootpw.SubString($rootpw.LastIndexOf(": ") + 2)
+$global:sshpw[$DevID] = $SSHpw.SubString($SSHpw.LastIndexOf(": ") + 2)
+$global:ex[$DevID] = $Expires.SubString($Expires.LastIndexOf(": ") + 2)
