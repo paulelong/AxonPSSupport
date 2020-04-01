8d09541c65f1e593e3a100e893636ff4f35a66d8
Paul E. Long <paulelong@outlook.com>
2020-04-01 09:46:19 -0400

    Copy file from ab3

---
+++ axcopy.ps1
@@ -0,0 +1,25 @@
+param 
+(
+    $DevID,
+    $Path
+)
+
+$exdate = Get-Date $ex[$DevID]
+$curdate = Get-Date
+$diff = $exdate - $curdate
+
+if($exdate -lt $curdate)
+{
+    Write-Host "Token has expired $diff ago"
+}
+else {
+    $diff = $exdate = $curdate
+    Write-Host "Token Expires in $diff"
+
+    Write-Host "$DevID fw=$($firmware[$DevID]) expires $($ex[$DevID])"
+    Write-Host "pw=$($sshpw[$DevID])"
+    Write-Host "supw=$($supw[$DevID])"
+    Write-Host "========================================================="
+    
+    scp axuser@$($DevID):$($Path) .
+}
