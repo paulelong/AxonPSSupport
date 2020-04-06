3051fe39dabb856bf870f8efe8c7d4337b248385
Paul E. Long <paulelong@outlook.com>
2020-04-06 18:16:28 -0400

    Add output about state

--- wslenable.ps1
+++ wslenable.ps1
@@ -7,8 +7,10 @@ param
 
     if($e -eq $true)
     {
+        Write-Host "Enabling WSL"
         Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
     } elseif ($enabled -eq $false){
+        Write-Host "Disabling WSL"
         Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
     }
     else {
